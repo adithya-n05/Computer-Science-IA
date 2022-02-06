8dc1f7d3508ad0b03569a2bbd008e0f371b65f8a
Adithya <adithya.narayanan01@gmail.com>
2022-02-07 01:33:51 +0800

    Created JFrames for login and create new account pages

---
+++ Product/EffortGradeGenerator/src/main/java/com/computerscienceia/effortgradegenerator/Person.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.computerscienceia.effortgradegenerator;
+
+
+public class Person {
+    private int id;
+    private int firstName;
+    private int lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(int firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(int lastName) {
+        this.lastName = lastName;
+    }
+    
+}
